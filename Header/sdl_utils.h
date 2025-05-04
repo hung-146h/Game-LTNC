d2f9b83c41057e7b84cd877d05e20b5f889c573b
hung-146h <hungphantoanlyhoa@gmail.com>
2025-05-04 22:37:20 +0700

    Update sdl_utils.h

--- Header/sdl_utils.h
+++ Header/sdl_utils.h
@@ -7,7 +7,7 @@
 
 void logErrorAndExit(const char* msg, const char* error);
 SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE);
-SDL_Renderer* createRenderer(SDL_Window* window, int SCREEN_WIDTH, int SCREEN_HEIGHT); // Thêm tham số
+SDL_Renderer* createRenderer(SDL_Window* window, int SCREEN_WIDTH, int SCREEN_HEIGHT); 
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 SDL_Texture* loadTexture(const char* filename, SDL_Renderer* renderer);
 SDL_Texture* createTextTexture(TTF_Font* font, const char* text, SDL_Color color, SDL_Renderer* renderer);
